607ab2dc881cf17d1dc034c95d57f25ff130a0e9
Dawn <dtrail76@gmail.com>
2023-09-12 13:17:38 +0100

    add project palette colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-blue": "#010143",
+        "dark-blue": "#03396c",
+        "accent-blue": "#005b96",
+        "lighter-blue": "#6497b1",
+        "lightest-blue": "#b3cde0",
+      },
+    },
   },
   plugins: [],
 };
