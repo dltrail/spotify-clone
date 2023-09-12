9db541e6a7f8e15de32feeacfe80a112a7beccb6
Dawn <dtrail76@gmail.com>
2023-09-12 13:41:57 +0100

    use subtitle instead of mapping over artists

--- src/components/SongCard.tsx
+++ src/components/SongCard.tsx
@@ -7,13 +7,10 @@ type Props = {
 function SongCard({ track }: Props) {
     const { title, key, artists, images, share } = track
     return (
-        <div className="flex flex-col items-center w-[220px] p-4 h-auto cursor-pointer bg-opacity-80 bg-white/5 backdrop-blur-sm text-lightest-blue rounded-lg text-sm">
+        <div className="flex flex-col w-[220px] text-left p-4 h-auto cursor-pointer bg-opacity-80 bg-white/5 backdrop-blur-sm text-lightest-blue rounded-lg text-sm">
             <img src={images.coverart} className="object-contain w-full mb-4" width="100%" height="auto" />
-            <p className="text-wrap capitalize font-bold text-sm">{title}</p>
-            {track.artists.map((artist) => (
-                <span key={artist.adamid} className="capitalize font-extralight text-xs">{artist.alias.replace('-', " ")}</span>
-            ))
-            }
+            <p className="text-wrap capitalize font-bold text-sm truncate">{title}</p>
+            <p className="capitalize font-extralight text-xs">{title}</p>
         </div >
     )
 }
