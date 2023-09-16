674a0f89fdefaf2b74c5726d299df549535257ec
D Trail <dtrail76@gmail.com>
2023-09-16 12:35:35 +0100

    comment out unused props

--- src/components/MusicPlayer/Controls.tsx
+++ src/components/MusicPlayer/Controls.tsx
@@ -1,11 +1,13 @@
 import { MdSkipNext, MdSkipPrevious } from 'react-icons/md';
-import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
+// import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
+import { BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
 
 type Props = {
   isPlaying: boolean, repeat: boolean, setRepeat: (prev: boolean) => void, shuffle: boolean, setShuffle: (prev: boolean) => void, currentSongs: [], handlePlayPause: () => void, handlePrevSong: () => void, handleNextSong: () => void, isActive: boolean
 }
 
-const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }: Props) => (
+// const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }: Props) => (
+const Controls = ({ isPlaying, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }: Props) => (
   <div className="flex items-center justify-around md:w-36 lg:w-52 2xl:w-80">
     {/* <BsArrowRepeat size={20} color={repeat ? 'red' : 'white'} onClick={setRepeat(!repeat)} className="hidden sm:block cursor-pointer" /> */}
     {currentSongs?.length && <MdSkipPrevious size={30} color="#FFF" className="cursor-pointer" onClick={handlePrevSong} />}
